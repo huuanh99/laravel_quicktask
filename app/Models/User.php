da76bdabe03ab40e356f47394dcf381fc9e4a39a
Tran Huu Anh <huuanhpro00@gmail.com>
2021-11-18 09:31:52 +0700

    create migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table='users';
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class, 'user_id');
+    }
 }
